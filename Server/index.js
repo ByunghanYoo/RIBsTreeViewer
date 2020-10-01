53f40d67c5a24d5c1e03ea51fe0ada03633c047a
Ryan Yoo <yoobh17@gmail.com>
2020-10-01 17:44:29 +0900

    Update Server/index.js

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,20 @@
-var server = require('ws').Server;
-var webSocketServer = new server({ port: 8080 });
+var server = require("ws").Server;
+var webSocketServer = new server({ port: 8875 });
 
-webSocketServer.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    console.log(message);
+webSocketServer.on("connection", (ws) => {
+  console.log("⚡️ WebSocket connected.");
+
+  ws.on("message", (message) => {
+    console.log(`📲 Message ${message}`);
 
     webSocketServer.clients.forEach((client) => {
       client.send(message);
     });
   });
 
-  ws.on('close', () => {
+  ws.on("close", () => {
+    console.log("💥 WebSocket disconnected.");
   });
 });
+
+console.log("🌞 RIBsTreeViewer WebSocket server is running...");
